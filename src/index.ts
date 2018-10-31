bbea2c08f4fe213c0a5b4ffa0e81c0b0c6a2c438
Christian Rackerseder <git@echooff.de>
2018-10-31 20:46:43 +0100

    🔐 Verify secret implementation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
-export function testMe() {
-  return 123;
-}
+import crypto from 'crypto';
+import { IncomingMessage } from 'http';
+import { text } from 'micro';
+
+export = async function verifySecret(
+  req: IncomingMessage,
+  secret: string,
+): Promise<boolean> {
+  const signature = req.headers['x-hub-signature'];
+  if (!signature) {
+    return false;
+  }
+  const body = await text(req);
+  const hmac = crypto.createHmac('sha1', secret);
+  hmac.update(body);
+  const calculated = `sha1=${hmac.digest('hex')}`;
+  return signature === calculated;
+};
