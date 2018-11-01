d5d48b9b5ed7c655ee32062fe97a929fb3addee3
Christian Rackerseder <git@echooff.de>
2018-11-01 22:52:27 +0100

    ♻️ Replace co-body with micro
    Because body parsing can only be done once with co-body

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { IncomingMessage } from 'http';
-import parse from 'co-body';
+import { text } from 'micro';
 import { verifySecret } from './verify';
 
 export = async (req: IncomingMessage, secret: string): Promise<boolean> => {
   const signature = req.headers['x-hub-signature'];
-  const body = await parse.text(req);
+  const body = await text(req);
   return verifySecret(body, secret, signature);
 };
--- test/integration/verify.test.ts
+++ test/integration/verify.test.ts
@@ -61,3 +61,25 @@ test('return "true" when secret is correct', async t => {
   });
   server.close();
 });
+
+test('should not hang when verify is called more than once', async t => {
+  t.plan(1);
+  const server = micro(async req => {
+    const valid = await verify(req, 'my-secret');
+    await verify(req, 'my-secret');
+    t.true(valid);
+    return '';
+  });
+  const url = await listen(server);
+  await got(url, {
+    method: 'POST',
+    headers: {
+      'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
+    },
+    json: true,
+    body: {
+      foo: 'bar',
+    },
+  });
+  server.close();
+});
