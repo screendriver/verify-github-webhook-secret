9466c1f5ff9c35144a1f3b5f538a25205b6ead19
Christian Rackerseder <git@echooff.de>
2019-11-17 14:02:13 +0100

    ♻️ Use Mocha instead of AVA in tests

--- test/unit/verify.test.ts
+++ test/unit/verify.test.ts
@@ -1,4 +1,4 @@
-import test from 'ava';
+import { expect } from 'chai';
 import { verify } from '../../src/verify';
 
 const body = JSON.stringify({
@@ -6,22 +6,24 @@ const body = JSON.stringify({
 });
 const signature = 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553';
 
-test('return "false" when signature is missing', t => {
-  const valid = verify(body, 'my-secret');
-  t.is(valid, false);
-});
+suite('verify', () => {
+  test('return "false" when signature is missing', () => {
+    const valid = verify(body, 'my-secret');
+    expect(valid).to.equal(false);
+  });
 
-test('return "false" when signature is an Array', t => {
-  const valid = verify(body, 'wrong-secret', signature);
-  t.is(valid, false);
-});
+  test('return "false" when signature is an Array', () => {
+    const valid = verify(body, 'wrong-secret', signature);
+    expect(valid).to.equal(false);
+  });
 
-test('return "false" when secret is wrong', t => {
-  const valid = verify(body, 'wrong-secret', signature);
-  t.is(valid, false);
-});
+  test('return "false" when secret is wrong', () => {
+    const valid = verify(body, 'wrong-secret', signature);
+    expect(valid).to.equal(false);
+  });
 
-test('return "true" when secret is correct', t => {
-  const valid = verify(body, 'my-secret', signature);
-  t.is(valid, true);
+  test('return "true" when secret is correct', () => {
+    const valid = verify(body, 'my-secret', signature);
+    expect(valid).to.equal(true);
+  });
 });
