9ff3be033fb4b55dc6e23e258631a5feed8798fb
Christian Rackerseder <git@echooff.de>
2021-11-10 19:21:57 +0100

    test: replace Mocha with AVA

--- test/integration/verify.test.ts
+++ test/integration/verify.test.ts
@@ -1,83 +1,85 @@
-import { assert } from 'chai';
+import test from 'ava';
 import micro from 'micro';
 import listen from 'test-listen';
 import got from 'got';
 import { verifySecret } from '../../src/index';
 
-suite('verify', function () {
-  test('return "false" when "x-hub-signature" header is missing', async function () {
-    const server = micro(async (req) => {
-      const valid = await verifySecret(req, 'my-secret');
-      assert.isFalse(valid);
-      return '';
-    });
-    const url = await listen(server);
-    await got(url, {
-      method: 'POST',
-      json: true,
-      body: {},
-    });
-    server.close();
+test('return "false" when "x-hub-signature" header is missing', async (t) => {
+  t.plan(1);
+  const server = micro(async (req) => {
+    const valid = await verifySecret(req, 'my-secret');
+    t.false(valid);
+    return '';
   });
+  const url = await listen(server);
+  await got(url, {
+    method: 'POST',
+    json: true,
+    body: {},
+  });
+  server.close();
+});
 
-  test('return "false" when secret is wrong', async function () {
-    const server = micro(async (req) => {
-      const valid = await verifySecret(req, 'wrong-secret');
-      assert.isFalse(valid);
-      return '';
-    });
-    const url = await listen(server);
-    await got(url, {
-      method: 'POST',
-      headers: {
-        'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
-      },
-      json: true,
-      body: {
-        foo: 'bar',
-      },
-    });
-    server.close();
+test('return "false" when secret is wrong', async (t) => {
+  t.plan(1);
+  const server = micro(async (req) => {
+    const valid = await verifySecret(req, 'wrong-secret');
+    t.false(valid);
+    return '';
   });
+  const url = await listen(server);
+  await got(url, {
+    method: 'POST',
+    headers: {
+      'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
+    },
+    json: true,
+    body: {
+      foo: 'bar',
+    },
+  });
+  server.close();
+});
 
-  test('return "true" when secret is correct', async function () {
-    const server = micro(async (req) => {
-      const valid = await verifySecret(req, 'my-secret');
-      assert.isTrue(valid);
-      return '';
-    });
-    const url = await listen(server);
-    await got(url, {
-      method: 'POST',
-      headers: {
-        'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
-      },
-      json: true,
-      body: {
-        foo: 'bar',
-      },
-    });
-    server.close();
+test('return "true" when secret is correct', async (t) => {
+  t.plan(1);
+  const server = micro(async (req) => {
+    const valid = await verifySecret(req, 'my-secret');
+    t.true(valid);
+    return '';
   });
+  const url = await listen(server);
+  await got(url, {
+    method: 'POST',
+    headers: {
+      'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
+    },
+    json: true,
+    body: {
+      foo: 'bar',
+    },
+  });
+  server.close();
+});
 
-  test('should not hang when verify is called more than once', async function () {
-    const server = micro(async (req) => {
-      const valid = await verifySecret(req, 'my-secret');
-      await verifySecret(req, 'my-secret');
-      assert.isTrue(valid);
-      return '';
-    });
-    const url = await listen(server);
-    await got(url, {
-      method: 'POST',
-      headers: {
-        'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
-      },
-      json: true,
-      body: {
-        foo: 'bar',
-      },
-    });
-    server.close();
+test('should not hang when verify is called more than once', async (t) => {
+  t.plan(1);
+  const server = micro(async (req) => {
+    const valid = await verifySecret(req, 'my-secret');
+    await verifySecret(req, 'my-secret');
+    t.true(valid);
+    return '';
   });
+  const url = await listen(server);
+  await got(url, {
+    method: 'POST',
+    headers: {
+      'X-Hub-Signature': 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553',
+    },
+    json: true,
+    body: {
+      foo: 'bar',
+    },
+  });
+  server.close();
 });
--- test/unit/verify.test.ts
+++ test/unit/verify.test.ts
@@ -1,4 +1,4 @@
-import { assert } from 'chai';
+import test from 'ava';
 import { verify } from '../../src/verify';
 
 const body = JSON.stringify({
@@ -6,24 +6,22 @@ const body = JSON.stringify({
 });
 const signature = 'sha1=30a233839fe2ddd9233c49fd593e8f1aec68f553';
 
-suite('verify', function () {
-  test('return "false" when signature is missing', function () {
-    const valid = verify(body, 'my-secret');
-    assert.isFalse(valid);
-  });
+test('return "false" when signature is missing', (t) => {
+  const valid = verify(body, 'my-secret');
+  t.false(valid);
+});
 
-  test('return "false" when signature is an Array', function () {
-    const valid = verify(body, 'wrong-secret', signature);
-    assert.isFalse(valid);
-  });
+test('return "false" when signature is an Array', (t) => {
+  const valid = verify(body, 'wrong-secret', signature);
+  t.false(valid);
+});
 
-  test('return "false" when secret is wrong', function () {
-    const valid = verify(body, 'wrong-secret', signature);
-    assert.isFalse(valid);
-  });
+test('return "false" when secret is wrong', (t) => {
+  const valid = verify(body, 'wrong-secret', signature);
+  t.false(valid);
+});
 
-  test('return "true" when secret is correct', function () {
-    const valid = verify(body, 'my-secret', signature);
-    assert.isTrue(valid);
-  });
+test('return "true" when secret is correct', (t) => {
+  const valid = verify(body, 'my-secret', signature);
+  t.true(valid);
 });
